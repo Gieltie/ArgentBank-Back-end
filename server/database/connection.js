f568c57e3cac50e9c2683b99e81b36df88c3b7fc
Gael Hofman <Gieltie@outlook.fr>
2023-10-14 20:40:53 +0200

    mise en route mongo atlas

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const databaseUrl =
-  process.env.DATABASE_URL || "mongodb://127.0.0.1/argentBankDB";
+  /* process.env.DATABASE_URL || "mongodb://127.0.0.1/argentBankDB"; */
+  process.env.DATABASE_URL ||
+  "mongodb+srv://gieltie:zelohOVK6kXUQ1EW@argentbank.semvo7h.mongodb.net/argentbank?retryWrites=true&w=majority";
 
 module.exports = async () => {
   try {
